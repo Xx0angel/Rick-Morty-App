e9627b7a5cb0c23905015f2ef38067218933043b
Angel Garcia <angeliux.jossue@gmail.com>
2023-02-06 22:42:06 -0600

    modified cards styles

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,7 +1,7 @@
 function Character({character}) {
     return (
-        <div className="text-center p-5">
-            <h2 className="fs-4">{character.name}</h2>
+        <div className="text-left p-5 bg-dark my-2 rounded-3 shadow-lg">
+            <h2 className="fs-4 text-center mb-4">{character.name}</h2>
             <img className="img-fluid rounded-pill mb-3" src={character.image} alt={character.name} />
             <p className="py-1">Origen: {character.origin.name}</p>
             <p>Estado: {character.status}</p>
